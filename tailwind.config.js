91af1e1b76a70d379c79c4620a23316fabe2e115
cluzstudio <cluzstudio@gmail.com>
2022-03-08 23:30:09 -0500

    support tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,104 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+    darkMode: 'class',
     theme: {
         extend: {
+            colors: {
+                green: colors.emerald,
+                yellow: colors.amber,
+                purple: colors.violet,
+                red: colors.red,
+                blue: colors.blue,
+                orange: colors.orange,
+                gray: colors.gray,
+                white: colors.white,
+                black: colors.black,
+                primary: colors.blue,
+                secondary: colors.orange,
+                success: colors.green,
+                info: colors.blue,
+                warning: colors.yellow,
+                danger: colors.red,
+                light: colors.gray,
+                dark: colors.black,
+                text: colors.gray,
+                background: colors.white,
+                border: colors.gray[200],
+                placeholder: colors.gray[200],
+                highlight: colors.blue[100],
+                hover: colors.blue[100],
+                focus: colors.blue[100],
+                active: colors.blue[100],
+                disabled: colors.gray[200],
+                'on-background': colors.white,
+                'on-surface': colors.black,
+                'on-primary': colors.white,
+                'on-secondary': colors.white,
+                'on-success': colors.white,
+                'on-info': colors.white,
+                'on-warning': colors.white,
+                'on-danger': colors.white,
+                'on-light': colors.black,
+                'on-dark': colors.white,
+                'on-text': colors.black,
+                'on-background-hover': colors.gray[100],
+                'on-background-focus': colors.gray[100],
+                'on-background-active': colors.gray[100],
+                'on-surface-hover': colors.black,
+                'on-surface-focus': colors.black,
+                'on-surface-active': colors.black,
+                'on-primary-hover': colors.white,
+                'on-primary-focus': colors.white,
+                'on-primary-active': colors.white,
+                'on-secondary-hover': colors.white,
+                'on-secondary-focus': colors.white,
+
+            },
+            typography: {
+                DEFAULT: {
+                    css: {
+                          p: {
+                           marginBottom: '0.25rem',
+                           marginTop: '0.25rem',
+                        },
+                    },
+                },
+            },
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            },
+              },
         },
+
     },
-    variants: {
-        extend: {
-            backgroundColor: ['active'],
+    content: [
+        './app/**/*.php',
+        './resources/**/*.html',
+        './resources/**/*.js',
+        './resources/**/*.jsx',
+        './resources/**/*.ts',
+        './resources/**/*.tsx',
+        './resources/**/*.php',
+        './resources/**/*.vue',
+        './resources/**/*.twig',
+    ],
+
+    typography: {
+        default: {
+            css: {
+                p: {
+                    marginBottom: '0.5rem',
+                    marginTop: '0.5rem',
+                }
+            }
         }
     },
-    purge: {
-        content: [
-            './app/**/*.php',
-            './resources/**/*.html',
-            './resources/**/*.js',
-            './resources/**/*.jsx',
-            './resources/**/*.ts',
-            './resources/**/*.tsx',
-            './resources/**/*.php',
-            './resources/**/*.vue',
-            './resources/**/*.twig',
-        ],
-        options: {
-            defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
-            whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
-        },
-    },
+
     plugins: [
         require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
     ],
 };
