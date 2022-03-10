fdb4505e09f322d19ec238f3cd16308bf9f5fe41
Omar Villafuerte <ovillafuerte.94@gmail.com>
2022-03-10 14:24:17 -0500

    Add migration for update columns in settings table

---
+++ database/migrations/2022_03_10_191639_make_columns_nullable_on_settings_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeColumnsNullableOnSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->string('logo', 128)->nullable()->change();
+            $table->string('favicon', 128)->nullable()->change();
+            $table->string('campains_open', 128)->nullable()->change();
+            $table->string('register_open', 128)->nullable()->change();
+            $table->string('qvapay_app_secret', 256)->nullable()->change();
+            $table->string('qvapay_app_id', 256)->nullable()->change();
+            $table->string('manual', 256)->nullable()->change();
+            $table->string('enzona', 256)->nullable()->change();
+            $table->string('stripe', 256)->nullable()->change();
+            $table->string('stripe_app_key', 256)->nullable()->change();
+            $table->string('stripe_app_secret', 256)->nullable()->change();
+            $table->string('enzona_client_key', 256)->nullable()->change();
+            $table->string('enzona_client_secret', 256)->nullable()->change();
+            $table->string('card_manual', 256)->nullable()->change();
+            $table->string('card_manual_info', 256)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->string('logo', 128)->nullable(false)->change();
+            $table->string('favicon', 128)->nullable(false)->change();
+            $table->string('campains_open', 128)->nullable(false)->change();
+            $table->string('register_open', 128)->nullable(false)->change();
+            $table->string('qvapay_app_secret', 256)->nullable(false)->change();
+            $table->string('qvapay_app_id', 256)->nullable(false)->change();
+            $table->string('manual', 256)->nullable(false)->change();
+            $table->string('enzona', 256)->nullable(false)->change();
+            $table->string('stripe', 256)->nullable(false)->change();
+            $table->string('stripe_app_key', 256)->nullable(false)->change();
+            $table->string('stripe_app_secret', 256)->nullable(false)->change();
+            $table->string('enzona_client_key', 256)->nullable(false)->change();
+            $table->string('enzona_client_secret', 256)->nullable(false)->change();
+            $table->string('card_manual', 256)->nullable(false)->change();
+            $table->string('card_manual_info', 256)->nullable(false)->change();
+        });
+    }
+}
